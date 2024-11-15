393f025d0d110cff3e408c5b267e62766a20ff7b
Vanderson Batista <vandersonb@gmail.com>
2024-11-15 06:36:35 -0300

    adicionado a lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: ListComponent
+}];
